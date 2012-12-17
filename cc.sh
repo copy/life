83951bea29128e7df1e8053ed69a9bfb28d2fd3c
Fabian Hemmer <aranae@web.de>
2012-12-18 00:31:14 +0100

    fixed tabs. they are gone now

--- cc.sh
+++ cc.sh
@@ -3,9 +3,9 @@ FILENAME="out.js"
 ls -l $FILENAME
 
 java -jar ~/.local/closure-compiler.jar \
-	--compilation_level ADVANCED_OPTIMIZATIONS\
-	--js_output_file $FILENAME\
-	--warning_level VERBOSE\
-	--js draw.js life.js formats.js main.js 
+    --compilation_level ADVANCED_OPTIMIZATIONS\
+    --js_output_file $FILENAME\
+    --warning_level VERBOSE\
+    --js draw.js life.js formats.js main.js 
 
 ls -l $FILENAME
