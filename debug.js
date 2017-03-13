e252c0d4373523a00fd5a91696ee9fd4097fcd32
copy <copy@copy.sh>
2017-03-13 18:37:45 -0500

    Clean whitespace and remove debug.js

--- debug.js
+++
@@ -1,4 +0,0 @@
-document.write("<script src=life.js></script>");
-document.write("<script src=formats.js></script>");
-document.write("<script src=draw.js></script>");
-document.write("<script src=main.js></script>");
